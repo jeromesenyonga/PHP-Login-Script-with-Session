d59a8f576a3a75856e6f1edf79e9c5885e1b5000
Jeromesenyonga <jeromesenyonga4@gmail.com>
2021-08-03 14:58:09 +0300

    Login form validation

--- User_Login_Session/login.php
+++ User_Login_Session/login.php
@@ -40,5 +40,24 @@
                 </div>
             </form>
         </div>
+        <script>
+            function (){
+                var $valid = true;
+                document.getElementById("user_info").innerHTML = "":
+                document.getElementById("password_info").innerHTML = "";
+
+                var userName = document.getElementById("user_name").value;
+                var password = document.getElementById("password").value;
+                if(userName == ""){
+                    document.getElementById("user_info").innerHTML = "required";
+                    $valid = false;
+                }
+                if(password == ""){
+                    document.getElementById("password").innerHTML = "required";
+                    $valid = false;
+                }
+                return $valid;
+            }
+        </script>
     </body>
 </html>
